cc2307df715fb2bd881f3291efd753cf3823932b
chulingliu <liuchuling@easyway.net.cn>
2021-03-08 14:18:43 +0800

    初始化

---
+++ springboot-thymeleaf/src/main/java/com/windy/thymeleaf/entity/User.java
@@ -0,0 +1,17 @@
+package com.windy.thymeleaf.entity;
+
+/**
+ * @Classname User
+ * @Description TODO
+ * @Date 2021/1/22 10:33
+ * @Created by liuch
+ */
+public class User {
+    private String id;
+
+    private String username;
+
+    private String passwd;
+
+
+}
